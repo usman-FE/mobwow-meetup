205dc7663a4cf8608e5447317623b6c33de40759
Muhammad Usman <ch.usman564@gmail.com>
2023-07-11 13:13:20 +0500

    Built Header and Hero description

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,3 +11,9 @@ bodyOverflow: function (modal) {
   document.querySelector('body').classList.add(modal ? 'overflow-hidden' : 'overflow-visible');
 }
 */
+
+// GRAY3 #D9D9D9
+// GRAY6 #707070
+// GRAY7 #212121
+// viridian #00798A
+// peach #F65858
