6b0ff8b245bc08dbfbcdffb0c065d313068bc425
Muhammad Usman <ch.usman564@gmail.com>
2023-07-17 10:16:00 +0500

    Built similer events section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,17 @@ bodyOverflow: function (modal) {
 */
 
 // GRAY1 #F6F7F8
+// GRAY2 #E6E6E6
 // GRAY3 #D9D9D9
 // GRAY4 #C5C4C4
+// GRAY5 #A2A2A2
 // GRAY6 #707070
 // GRAY7 #212121
 // viridian #00798A
 // viridian hover #008fa3
-// peach #F65858
+// PEACH #F65858
 // DARD GOLD #7C6F50
+// OLD GOLD #947F5F
+// GREEN #07B203
 
 // px-6 sm:px-4 xl:px-0 md:max-w-[1120px] w-full mx-auto
